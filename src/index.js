ee75876f91512c5eeebd145c36ed9ea66aa81bad
lilivilch <109415499+lilivilch@users.noreply.github.com>
2023-07-25 23:48:04 +0500

    new mark

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Users from './components/users';
+import element from './components/users';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <Users />
   </React.StrictMode>
 );
 
